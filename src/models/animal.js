4f9493cdfdf82547d9ef5e05a33c7d5115e007cb
SebastianSC <sebassandoc@gmail.com>
2024-09-11 18:00:02 -0500

    cambios routes

--- src/models/animal.js
+++ src/models/animal.js
@@ -1,21 +1,20 @@
-const parser = require("body-parser");
-const express = require('express');
-const app = express();
-const port = 3000;
-const animalRoutes = require("./routes/animal");
-const mongoose = require("mongoose");
-require('dotenv').config();
-app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
-app.use(parser.json()); // transforma los datos a formato JSON
-//Gestión de las rutas usando el middleware
-app.use("/api", animalRoutes);
-app.use(express.json());
-//Conexión a la base de datos
-mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log("Conexión exitosa"))
-    .catch((error) => console.log(error));
-//Conexión al puerto
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+const mongoose = require("mongoose"); // importando el componente mogoose
+const animalSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+    },
+    edad: {
+        type: Number,
+        required: true,
+    },
+    tipo: {
+        type: String,
+        required: true,
+    },
+    fecha: {
+        type: Date,
+        requiered: true,
+    }
 });
+module.exports = mongoose.model("Animal", animalSchema);
--- src/routes/animal.js
+++ src/routes/animal.js
@@ -9,4 +9,41 @@ router.post("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+
+router.get("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+router.put("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Eliminar un animal por su id
+
+router.delete("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
+
+
 module.exports = router;
