b62a397ab38fe365e6af9f49dcfc10e4c3dfc710
sebassandoc <sebassandoc@gmail.com>
2024-10-02 17:30:11 -0500

    JWT

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
+const verifyToken = require('./validate_token');
+
 //Nuevo animal
 router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
@@ -18,6 +20,13 @@ router.get("/animals/:id", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+router.get("/animals",verifyToken, (req, res) => {
+    
+    animalSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
 router.put("/animals/:id", (req, res) => {
     const { id } = req.params;
